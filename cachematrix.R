23e4e25ab8f3a5e774c4f05e24475b59f556d677
hroshan <46976067+hroshan@users.noreply.github.com>
2020-10-04 21:04:38 -0500

    here is my initial solution for the third week's assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Here are two functions that enable us to cache a matrix inverse such that the
+## overall processing/computing time/load decreases when we use an inverse of a 
+## unchanged matrix more than one time. 
 
-## Write a short comment describing this function
+## The first function, makecacheMatrix, caches the inverse of a special "matrix"
+## that the function creates.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set<- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## The second function that follows, cacheSolve, calculate the inverse of the 
+## special "matrix" that is provided by the above function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  matrix <- x$get()
+  s <- solve(matrix, ...)
+  x$setsolve(s)
+  s
 }
